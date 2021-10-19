a026792d45df0e2ad120818b613b7d28fddd2c63
zhaochaochao <450959507@qq.com>
2021-10-19 10:05:51 +0800

    Add more for youtube dnn.

--- recall/youtube_dnn/preprocess.py
+++ recall/youtube_dnn/preprocess.py
@@ -10,27 +10,35 @@ def gen_data_set(data, negsample=0):
 
     train_set = []
     test_set = []
-    for reviewerID, hist in tqdm(data.groupby('user_id')):
-        pos_list = hist['movie_id'].tolist()
-        rating_list = hist['rating'].tolist()
+    for reviewerID, hist in tqdm(data.groupby('user_id')):  # 遍历每个用户
+        pos_list = hist['movie_id'].tolist()  # 观看历史
+        rating_list = hist['rating'].tolist()  # 评分历史
 
         if negsample > 0:
+            # 如果需要负采样，则从没看过的视频中随机采样，采样的比率由negsample决定
             candidate_set = list(set(item_ids) - set(pos_list))
             neg_list = np.random.choice(candidate_set, size=len(pos_list) * negsample, replace=True)
-        for i in range(1, len(pos_list)):
+        for i in range(1, len(pos_list)):  # 遍历每个视频构造训练和测试样本
             hist = pos_list[:i]
             if i != len(pos_list) - 1:
-                train_set.append((reviewerID, hist[::-1], pos_list[i], 1, len(hist[::-1]), rating_list[i]))
+                # 当前用户的正样本，观看历史列表中每个视频可以作为前面视频观看历史的训练学习目标
+                train_set.append((reviewerID,
+                                  hist[::-1],  # 该训练样本的输入是当前视频之前的视频列表
+                                  pos_list[i],  # 该训练样本的输出是当前视频id
+                                  1,  # 1代表正样本
+                                  len(hist[::-1]),  # 该样本的输入历史长度
+                                  rating_list[i]))  # 评级没用到
                 for negi in range(negsample):
+                    # 当前用户的负样本
                     train_set.append((reviewerID, hist[::-1], neg_list[i * negsample + negi], 0, len(hist[::-1])))
             else:
+                # 当前用户的测试样本，把历史列表最后一个视频和之前的观看历史作为该用户的测试数据
                 test_set.append((reviewerID, hist[::-1], pos_list[i], 1, len(hist[::-1]), rating_list[i]))
 
-    # random.shuffle(train_set)
-    # random.shuffle(test_set)
+    random.shuffle(train_set)
+    random.shuffle(test_set)
 
-    print(len(train_set[0]), len(test_set[0]))
-
+    # print(len(train_set[0]), len(test_set[0]))
     return train_set, test_set
 
 
--- recall/youtube_dnn/youtube_dnn.py
+++ recall/youtube_dnn/youtube_dnn.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import os
+import numpy as np
+import faiss
 from deepctr.feature_column import SparseFeat, VarLenSparseFeat
 from preprocess import gen_data_set, gen_model_input
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import Model
+from tqdm import tqdm
 
 from deepmatch.models import *
 from deepmatch.utils import sampledsoftmaxloss
+from deepmatch.utils import recall_N
 
 MODEL_PATH = "/tmp/youtube_dnn/"
 CKPT_PATH = os.path.join(MODEL_PATH, "checkpoint.h5")
@@ -15,6 +19,7 @@ SAVER_PATH = os.path.join(MODEL_PATH, "model_saver")
 DATA_PATH = "./"
 SEQ_LEN = 50  # Max movie history sequence len.
 NEG_SAMPLE = 1  # The rate of negative sample.
+EMBEDDING_DIM = 32  # The embedding dimension len.
 
 
 def gen_feature():
@@ -27,53 +32,45 @@ def gen_feature():
 
     data = pd.merge(pd.merge(ratings, movies), user)  # .iloc[:10000]
 
-    sparse_features = ["movie_id", "user_id",
-                       "gender", "age", "occupation", "zip", ]
-
     # Label Encoding for sparse features,and process sequence features with `gen_date_set` and `gen_model_input`
-    features = ['user_id', 'movie_id', 'gender', 'age', 'occupation', 'zip']
+    sparse_features = ["movie_id", "user_id", "gender", "age", "occupation", "zip", ]
     feature_max_idx = {}
-    for feature in features:
+    for feature in sparse_features:
         lbe = LabelEncoder()
-        data[feature] = lbe.fit_transform(data[feature]) + 1
-        feature_max_idx[feature] = data[feature].max() + 1
+        data[feature] = lbe.fit_transform(data[feature]) + 1  # 规范每个特征的label到1到n_class之间
+        feature_max_idx[feature] = data[feature].max() + 1  # 记录每个特征最大标签+1，用于keras.layers.Embedding传入的input_dim参数
 
     user_profile = data[["user_id", "gender", "age", "occupation", "zip"]].drop_duplicates('user_id')
-
-    item_profile = data[["movie_id"]].drop_duplicates('movie_id')
-
     user_profile.set_index("user_id", inplace=True)
-
-    user_item_list = data.groupby("user_id")['movie_id'].apply(list)
+    item_profile = data[["movie_id"]].drop_duplicates('movie_id')
 
     train_set, test_set = gen_data_set(data, NEG_SAMPLE)
 
     train_model_input, train_label = gen_model_input(train_set, user_profile, SEQ_LEN)
     test_model_input, test_label = gen_model_input(test_set, user_profile, SEQ_LEN)
-    return train_model_input, train_label, test_model_input, test_label, feature_max_idx
+    return train_model_input, train_label, test_model_input, test_label, feature_max_idx, item_profile, test_set
 
 
 def def_model_and_train(train_model_input, train_label, feature_max_idx):
     # count #unique features for each sparse field and generate feature config for sequence feature
-    embedding_dim = 32
 
     user_feature_columns = [SparseFeat('user_id', feature_max_idx['user_id'], 16),
                             SparseFeat("gender", feature_max_idx['gender'], 16),
                             SparseFeat("age", feature_max_idx['age'], 16),
                             SparseFeat("occupation", feature_max_idx['occupation'], 16),
                             SparseFeat("zip", feature_max_idx['zip'], 16),
-                            VarLenSparseFeat(SparseFeat('hist_movie_id', feature_max_idx['movie_id'], embedding_dim,
+                            VarLenSparseFeat(SparseFeat('hist_movie_id', feature_max_idx['movie_id'], EMBEDDING_DIM,
                                                         embedding_name="movie_id"), SEQ_LEN, 'mean', 'hist_len'),
                             ]
 
-    item_feature_columns = [SparseFeat('movie_id', feature_max_idx['movie_id'], embedding_dim)]
+    item_feature_columns = [SparseFeat('movie_id', feature_max_idx['movie_id'], EMBEDDING_DIM)]
 
     # Define model.
     import tensorflow as tf
     if tf.__version__ >= '2.0.0':
         tf.compat.v1.disable_eager_execution()
     model = YoutubeDNN(user_feature_columns, item_feature_columns, num_sampled=100,
-                       user_dnn_hidden_units=(128, 64, embedding_dim))
+                       user_dnn_hidden_units=(128, 64, EMBEDDING_DIM))
     model.compile(optimizer="adam", loss=sampledsoftmaxloss)  # "binary_crossentropy")
     model.summary()
 
@@ -90,10 +87,52 @@ def def_model_and_train(train_model_input, train_label, feature_max_idx):
     if not os.path.exists(SAVER_PATH):
         os.makedirs(SAVER_PATH)
 
+    print(model.user_embedding.shape)
+    print(model.item_embedding.shape)
+
     history = model.fit(train_model_input, train_label,  # train_label,
                         batch_size=512, epochs=20, verbose=1, validation_split=0.0, callbacks=callbacks)
 
+    user_embedding_model = Model(inputs=model.user_input, outputs=model.user_embedding)
+    item_embedding_model = Model(inputs=model.item_input, outputs=model.item_embedding)
+    return user_embedding_model, item_embedding_model
+
+
+def evaluate(user_embedding_model, item_embedding_model, user_input, item_profile, test_set):
+    test_true_label = {line[0]: [line[2]] for line in test_set}
+    print('test_true_label: {}'.format(test_true_label))
+
+    item_input = {"movie_id": item_profile['movie_id'].values, }
+    user_embeds = user_embedding_model.predict(user_input, batch_size=2 ** 12)
+    item_embeds = item_embedding_model.predict(item_input, batch_size=2 ** 12)
+    print('user_embeds.shape: {}'.format(user_embeds.shape))
+    print('item_embeds.shape: {}'.format(item_embeds.shape))
+
+    # Create faiss index database.
+    index = faiss.IndexFlatIP(EMBEDDING_DIM)  # 建立内积距离作为相似度计算的向量索引数据库
+    print('index.is_trained: {}'.format(index.is_trained))
+    index.add(item_embeds)
+    print('index.ntotal: {}'.format(index.ntotal))
+    distance, idx = index.search(np.ascontiguousarray(user_embeds), 50)
+    print('distance.shape: {}'.format(distance.shape))
+    print('idx.shape: {}'.format(idx.shape))
+    s = []
+    hit = 0
+    for i, uid in tqdm(enumerate(user_input['user_id'])):
+        pred = [item_profile['movie_id'].values[x] for x in idx[i]]
+        filter_item = None
+        recall_score = recall_N(test_true_label[uid], pred, N=50)
+        s.append(recall_score)
+        if test_true_label[uid] in pred:
+            hit += 1
+
+    print("")
+    print("recall", np.mean(s))
+    print("hit rate", hit / len(user_input['user_id']))
+
 
 if __name__ == '__main__':
-    train_model_input, train_label, test_model_input, test_label, feature_max_idx = gen_feature()
-    def_model_and_train(train_model_input, train_label, feature_max_idx)
+    train_model_input, train_label, test_model_input, test_label, feature_max_idx, item_profile, test_set \
+        = gen_feature()
+    user_embedding_model, item_embedding_model = def_model_and_train(train_model_input, train_label, feature_max_idx)
+    evaluate(user_embedding_model, item_embedding_model, test_model_input, item_profile, test_set)
